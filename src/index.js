8a0acafe803658a30b4bc8cd00eef8bc54da5edf
Leonardo Brito <f.leobrito@gmail.com>
2019-06-11 10:30:32 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://instagram-backend:instagram-backend@cluster0-eicsh.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use(cors());
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+app.use(require("./routes"));
+
+server.listen(3000);
